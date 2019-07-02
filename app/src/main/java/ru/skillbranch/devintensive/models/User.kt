e84cfd71f4203871177550becbf93660cee40ba9
Roman Barlos <darkgenius@nextmail.ru>
2019-07-02 22:43:37 +0300

    task1

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,24 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.*
+
+data class User(val id : String,
+           var firstName : String?,
+           var lastName : String?,
+           var avatar : String?,
+           var rating : Int = 0,
+           var respect : Int = 0,
+           var lastVisit : Date? = Date(),
+           var isOnline : Boolean = false) {
+
+    companion object Factory {
+        private var lastId: Int = -1
+        fun makeUser(fullName: String?): User {
+            lastId++
+            val parts : List<String>? = fullName?.split(' ')
+            val firstName: String = parts?.getOrNull(0) ?: ""
+            val lastName: String = parts?.getOrNull(1) ?: ""
+            return User(id = "$lastId", firstName = firstName, lastName = lastName, avatar = null)
+        }
+    }
+}
